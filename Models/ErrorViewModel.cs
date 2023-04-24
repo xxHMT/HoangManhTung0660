0556e36f92d4261809943dcb2c9d4aead3b3d358
xxHMT <algoroku@gmail.com>
2023-04-24 15:06:05 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HoangManhTung0660.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
